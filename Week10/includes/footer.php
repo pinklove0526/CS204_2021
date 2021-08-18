297bf5c857752bd6f262c59b6be01d4110127855
Jellyfish Ng <72180039+pinklove0526@users.noreply.github.com>
2021-08-18 11:24:16 +0700

    Add files via upload

--- Week10/includes/footer.php
+++ Week10/includes/footer.php
@@ -1,50 +1,48 @@
         <!-- Footer -->
         <footer class="bg-dark text-center text-white mt-5">
-            <!-- Grid container -->
-            <div class="container p-4">
-                <!-- Section: Social media -->
-                <section class="mb-4">
-                    <!-- Facebook -->
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><ion-icon name="logo-facebook"></ion-icon></a>
-                    <!-- Twitter -->
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><ion-icon name="logo-twitter"></ion-icon></a>
-                    <!-- Google -->
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><ion-icon name="logo-google"></ion-icon></a>
-                    <!-- Instagram -->
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><ion-icon name="logo-instagram"></ion-icon></a>
-                    <!-- LinkedIn -->
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><ion-icon name="logo-linkedin"></ion-icon></a>
-                    <!-- GitHub -->
-                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><ion-icon name="logo-github"></ion-icon></a>
-                </section>
-                <!-- Section: Text -->
-                <section class="mb-4"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></section>
-                <!-- Section: Text -->
+        <!-- Grid container -->
+        <div class="container p-4">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+                <!-- Facebook -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><ion-icon name="logo-facebook"></ion-icon></a>
+                <!-- Twitter -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><ion-icon name="logo-twitter"></ion-icon></a>
+                <!-- Google -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><ion-icon name="logo-google"></ion-icon></a>
+                <!-- Instagram -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><ion-icon name="logo-instagram"></ion-icon></a>
+                <!-- LinkedIn -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><ion-icon name="logo-linkedin"></ion-icon></a>
+                <!-- GitHub -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><ion-icon name="logo-github"></ion-icon></a>
+            </section>
+            <!-- Section: Text -->
+            <section class="mb-4"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></section>
+            <!-- Section: Text -->
 
-                <!-- Section: Links -->
-                <section class="">
-                    <!-- Grid row -->
+            <!-- Section: Links -->
+            <section class="">
+                <!-- Grid row -->
 
-                    <!-- Grid row -->
-                </section>
-                <!-- Section: Links -->
-            </div>
-            <!-- Grid container -->
+                <!-- Grid row -->
+            </section>
+            <!-- Section: Links -->
+        </div>
+        <!-- Grid container -->
 
-            <!-- Copyright -->
-            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">© <?php echo date("Y/m/d H:i:s"); ?> Copyright: <a class="text-white" href="#">ITEC</a></div>
-            <!-- Copyright -->
-        </footer>
-        <!-- Footer -->
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">© <?php echo date("Y/m/d H:i:s"); ?> Copyright: <a class="text-white" href="#">ITEC</a></div>
+        <!-- Copyright -->
         <!-- Optional JavaScript; choose one of the two! -->
 
         <!-- Option 1: Bootstrap Bundle with Popper -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 
         <!-- Option 2: Separate Popper and Bootstrap JS -->
         <!--
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
         -->
         <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
--- Week10/includes/header.php
+++ Week10/includes/header.php
@@ -6,13 +6,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
         <link rel="stylesheet" href="css/style.css">
         <title>ITEC Blog 2021</title>
     </head>
     <body>
-        <!-- fixed-top | sticky-top | fixed-bottom -->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+       <!-- fixed-top | sticky-top | fixed-bottom -->
+       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
                 <a class="navbar-brand" href="index.php">ITEC Blog 2021</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
